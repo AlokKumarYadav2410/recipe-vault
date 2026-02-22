1a545cce2eb0ba424cef327e66d71f21a9a75124
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-02-22 14:06:19 +0530

    add pages and components for RecipeVault; update dependencies and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const App = () => {
   return (
-    <div>
-      
+    <div className='py-5 px-[10%] bg-gray-800 w-screen h-screen font-thin text-white' >
+      Welcome to RecipeVault
     </div>
   )
 }
