eba04fbb612ebd5b43014511f412104533d1670b
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-02-28 21:02:52 +0530

    add navigation button to create a recipe when no recipes are available

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useRecipes } from "../context/RecipeContext";
 import RecipeCard from "../components/RecipeCard";
 import ScrollReveal from "../components/ScrollReveal";
+import { useNavigate } from "react-router-dom";
 
 const Recipes = () => {
   const { data } = useRecipes();
+
+  const navigate = useNavigate();
 
   if (data.length === 0) {
     return (
       <div className="text-center py-20 text-gray-400">
         <h2 className="text-2xl">No recipes yet 👨‍🍳</h2>
         <p className="mt-2">Start by creating your first recipe!</p>
+        <button onClick={() => navigate("/create-recipe")} className="mt-4 px-4 py-2 cursor-pointer bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
+          Create Recipe
+        </button>
       </div>
     );
   }
